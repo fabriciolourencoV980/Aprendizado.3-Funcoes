33fedfe20e6f2e0ac94b5d8c1affb39525759ae7
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-11-04 15:06:08 -0300

    .

--- Funções/6thisEbind2.js
+++ Funções/6thisEbind2.js
@@ -11,3 +11,5 @@ function pessoa(){
 new pessoa 
 
 //setInterval() é uma função em JavaScript que executa repetidamente uma função ou bloco de código em um intervalo de tempo especificado (em milissegundos), até que seja cancelada com clearInterval(). É útil para tarefas que precisam ser executadas de forma repetitiva, como atualizações de interface ou contadores.
+
+//O método bind() em JavaScript cria uma nova função com o valor de this fixado para um valor específico, o que ajuda a garantir que this mantenha o mesmo valor mesmo quando a função é chamada em um contexto diferente. Isso é útil, especialmente ao passar métodos de objetos como callbacks, onde o valor de this pode mudar.
