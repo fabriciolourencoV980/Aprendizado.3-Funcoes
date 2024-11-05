8ad0f5eb1b41d71f52871affe398adc73f91f8c1
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-11-05 16:55:11 -0300

    .

--- Funções/7Funções_arrow.js
+++ Funções/7Funções_arrow.js
@@ -6,4 +6,13 @@ dobro = (a) => {
     return 2 * a 
 };
 
-dobro = a => 2 * a;
+dobro = a => 2 * a; // return implicito
+console.log(dobro(Math.PI));
+
+let ola = function (){
+    console.log('Olá')
+};
+
+ola = () => 'Olá'
+ola = _ => 'Olá' //Possui parametro
+console.log(ola());
