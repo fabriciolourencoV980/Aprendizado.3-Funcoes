499349bc73f5317e172ce4df10567f8c8454136e
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-11-08 10:33:46 -0300

    .

---
+++ Funções/21atv2.js
@@ -0,0 +1,17 @@
+let a = 1
+let b = 1
+let c = 1
+
+if(a === b && b === c ){
+    console.log('Equilátero')
+} 
+else if (a !== b && b !== c && a !== c){
+    console.log('Escaleno')
+}
+else{
+    console.log('Isóceles')
+} 
+
+
+
+
