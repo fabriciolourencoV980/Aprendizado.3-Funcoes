fffa7eb0737da0d13d6bf8dc9ebaba9f3fa58e8c
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-11-08 08:05:39 -0300

    .

---
+++ Funções/15TiposDeclaração.js
@@ -0,0 +1,17 @@
+//Function Declaration
+
+function soma (x, y){
+    return x + y 
+};
+
+//Function expression
+
+const sub = function (x, y){
+    return x - y
+}; 
+
+//Named function expression 
+
+const mult = function mult(x, y){
+    return x * y
+};
